5cb34f5d3e9ac5c0d75cf9167d5ae0b6aa434fc5
irhmaan <rehmaan@7191IMDADU4764L.mind.motherson.com>
2026-04-07 05:02:17 +0000

    4WD urdf added and fixed launch issues. Added rviz config to load the urdf in rviz , without it it caused issues

--- src/fourwd_robot/launch/display.launch.py
+++ src/fourwd_robot/launch/display.launch.py
@@ -1,50 +1,74 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
-from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, Command , FileContent
 from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+
+
 
 def generate_launch_description():
-    pkg_share = FindPackageShare(package='fourwd_robot').find('fourwd_robot')
-    
-    urdf_model = LaunchConfiguration('urdf_model')
-    rviz_config = LaunchConfiguration('rviz_config')
 
-    declare_urdf_cmd = DeclareLaunchArgument(
-        name='urdf_model',
-        default_value=PathJoinSubstitution([pkg_share, 'urdf', 'fourwd_robot.urdf']),
-        description='Absolute path to robot urdf file'
+    # URDF path
+    urdf_path = PathJoinSubstitution([
+        FindPackageShare('fourwd_robot'),
+        'urdf',
+        'fourwd_robot.urdf'
+    ])
+
+    #rviz path
+    rviz_config_path = PathJoinSubstitution([
+    FindPackageShare('fourwd_robot'),
+    'rviz',
+    'fourwd_robot.rviz'
+    ])
+
+    # Launch arguments
+    gui_arg = DeclareLaunchArgument(
+        name='gui',
+        default_value='true',
+        choices=['true', 'false'],
+        description='Enable joint_state_publisher_gui'
     )
 
-    declare_rviz_cmd = DeclareLaunchArgument(
-        name='rviz_config',
-        default_value=PathJoinSubstitution([pkg_share, 'rviz', 'view_robot.rviz']),
-        description='Absolute path to rviz config file'
+    model_arg = DeclareLaunchArgument(
+        name='model',
+        default_value=urdf_path,
+        description='Path to URDF file'
     )
 
-    robot_state_publisher_node = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        arguments=[urdf_model]
-    )
+    # robot_description
+    robot_description = {
+        'robot_description': ParameterValue(
+            FileContent(LaunchConfiguration('model')),
+            value_type=str
+        )
+    }
 
-    joint_state_publisher_gui_node = Node(
-        package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui',
-        name='joint_state_publisher_gui'
-    )
+    return LaunchDescription([
 
-    rviz_node = Node(
+        gui_arg,
+        model_arg,
+
+        # Robot State Publisher
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            parameters=[robot_description]
+        ),
+        
+
+        # Joint State Publisher GUI
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui'
+        ),
+        # Rviz loaded with fourwd.rviz   
+        Node(
         package='rviz2',
         executable='rviz2',
         name='rviz2',
-        arguments=['-d', rviz_config]
+        arguments=['-d', rviz_config_path],
+        output='screen'
     )
-
-    return LaunchDescription([
-        declare_urdf_cmd,
-        declare_rviz_cmd,
-        robot_state_publisher_node,
-        joint_state_publisher_gui_node,
-        rviz_node
     ])
